3972550f2e1b6627cc681699283ca7bcf3139669
yuhonakayama <yuhoimaizumi@gmail.com>
2024-10-13 00:13:14 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $Array = ['name' => 'onion', 'price' => '200', 'weight' => '160' ];
+
+    print_r($Array);
+    ?>
+  </p>
+</body>
+</html>
